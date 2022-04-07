d90a23f3dd97e2c7f9f2f2f27407a2a6a3f82d67
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-04-07 22:11:09 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/scss/App.css'
+import Nav from './Components/structure/Nav'
+import Main from './Components/structure/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
