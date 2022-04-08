e60acd9dd20ceeb3631285e7461e76ea4d51c177
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-04-08 22:23:55 +0200

    Add files via upload

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+  return (
+    <main className='main'>
+      <div className='gird'>
+        <section>Hello</section>
+        <section>Hello2</section>
+        <section>Hello3</section>
+      </div>
+    </main>
+  )
+}
+
+export default Main
