b86b9dda9e37344407535b13405ce179209a4351
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-04-24 22:34:03 +0200

    Add files via upload

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,10 +2,9 @@ import React from 'react'
 import './Header.css'
 import Nav from '../Nav/Nav'
 
-const Header = (e) => {
+const Header = () => {
   return (
     <header className='header'>
-      // tutaj coś dodać. //tak
       <div className='headerInfo'></div>
       <Nav />
     </header>
