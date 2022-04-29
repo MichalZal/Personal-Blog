6832b327ce833155f53866e7408643074cd33a89
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-04-29 21:39:36 +0200

    Add files via upload

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,18 @@
 import React from 'react'
+import { BsGithub, BsFacebook } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai'
 import './Nav.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Nav = () => {
   function toggleHamMenu(e) {
     const hamMenu = document.querySelector('.hamMenu')
-
     const navbarLinks = document.querySelector('.navbarLinks')
-
     hamMenu.classList.toggle('hamMenu-active')
-    
     navbarLinks.classList.toggle('navbarLinks-active')
   }
 
-
   return (
-    <>
+    <div className='color'>
       <nav className='navbar'>
         <div className='navbarLogo'>
           <a href="../../App.js">
@@ -24,8 +21,21 @@ const Nav = () => {
         </div>
         <div className='navbarLinks'>
           <div className='socialLinks'>
-            <div className='facebook'>facebook</div>
-            <div className='github'>github</div>
+            <div className='facebook'>
+              <a href="https://www.facebook.com/Michalzalewsk">
+                <BsFacebook className='face-logo'/>
+              </a>
+            </div>
+            <div className='github'>
+              <a href="https://github.com/MichalZal">
+                <BsGithub className='git-logo'/>
+              </a>
+            </div>
+            <div className='instagram'>
+              <a href="https://www.instagram.com/chraabaszcz/">
+                <AiFillInstagram className='insta-logo'/>
+              </a>
+            </div>
           </div>
           <ul className='ul'>
             <li className='navbarLink'> <a href='../../App.js'> Strona Główna</a></li>
@@ -40,12 +50,12 @@ const Nav = () => {
           <span className="bar" id="bar3"></span>
         </div>
         <div className='info'>
-          <p> Hello. My name is Michał Zalewski. I really like front-end development, but I can also do back-end.
-            My favourite technologies are React and NodeJS 
+          <p className='about'> <span className='link'>Hello. My name is Michał Zalewski. I really like front-end development, but I can also do back-end.
+            My favourite technologies are <span>React</span> and <span>NodeJS</span></span>
           </p>
         </div>
       </nav>
-    </>
+    </div>
   )
 }
 
